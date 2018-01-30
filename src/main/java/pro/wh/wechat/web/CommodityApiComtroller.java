b208a6eb2297debd31dc404d321129c9f34fe27b
joker0198 <joker0198@163.com>
2018-01-30 10:13:08 +0800

    测试

--- src/main/java/pro/wh/wechat/web/CommodityApiComtroller.java
+++ src/main/java/pro/wh/wechat/web/CommodityApiComtroller.java
@@ -61,7 +61,7 @@ public class CommodityApiComtroller {
         return gson.toJson(result);
     }
 
-    //
+    //ss
     @RequestMapping(value = "search", method = RequestMethod.GET)
     public String search(String title){
         logger.info("title:" + title);
